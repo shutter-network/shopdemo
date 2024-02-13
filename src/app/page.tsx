49a75aba291b3d6131f9e8c9792f416a7d5b9e60
Konrad Feldmeier <konrad@brainbot.com>
2024-02-13 15:20:20 +0100

    Return provider from connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,31 @@ async function connect() {
         const provider = new ShutterProvider(options, window.ethereum.url);
         console.log(provider);
         window.provider = provider;
-  
+        return provider;
+
     } else {
         console.log("No wallet");  
+    }
+}
+
+export function connectionInfo() {
+    // while (!window.provider) {
+    //     (async () => await connect());
+    // }
+    if (window.provider) {
+        const info = window.provider.getBlock();
+        return (info);
+    } else {
+        const info = "No connection";
+        return (info);
     }
 }
 
 export default function Home() {
     return (
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <button className="button" onClick={connect}>Connect</button>
+                <button type="button" className="button" onClick={connect}>Connect</button>
+                { connectionInfo() }
             </main>
            );
 }
