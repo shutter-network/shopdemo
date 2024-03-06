726f3ff5d32cba17246d5b438d137b1bc1d4c606
Konrad Feldmeier <konrad@brainbot.com>
2024-03-06 13:41:23 +0100

    Calldata as textarea

--- src/app/Transaction.tsx
+++ src/app/Transaction.tsx
@@ -38,7 +38,7 @@ class Transaction extends Component {
           className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-wrap break-words"
         />
         <label htmlFor="txdata">Data:</label>
-        <input
+        <textarea
           type="input"
           id="txdata"
           value={this.state.txdata}
