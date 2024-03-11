2dd6c3a4e2882faf004ceb55c2743126e3dd6baf
Konrad Feldmeier <konrad@brainbot.com>
2024-03-11 12:49:36 +0100

    Show ABI button next to Receiver

--- src/app/Transaction.tsx
+++ src/app/Transaction.tsx
@@ -9,27 +9,32 @@ class Transaction extends Component {
       txto: "",
       txvalue: 1,
       txdata: "",
+      receiverIsContract: true,
     };
   }
 
   renderTransaction() {
     return (
       <div>
-        <label htmlFor="txto">Receiver:</label>
-        <BlockiesSvg
-          address={this.state.txto}
-          className="receiver-icon w-px-40 h-px-40 float-right"
-          key={this.state.txto}
-          scale={5}
-        />
+        <div className="grid grid-cols-8">
+        <label htmlFor="txto" className="col-span-8">Receiver:</label>
         <input
           type="input"
           id="txto"
           value={this.state.txto}
           onChange={(event) => this.setState({ txto: event.target.value })}
-          className="block w-3/4 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-wrap break-words"
+          className="col-span-6 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-wrap break-words"
         />
-        <label htmlFor="txvalue">Value:</label>
+          <button
+            className="btn col-span-2"
+            type="btn"
+            onClick={(evt) => {evt.preventDefault(); this.overlay.current.style.display = "block" }}
+            style={{ display: this.state.receiverIsContract ? "inline-block": "hidden" }}
+          >
+            Use ABI
+          </button>
+        </div>
+        <label htmlFor="txvalue">Value (in wei):</label>
         <input
           type="input"
           id="txvalue"
